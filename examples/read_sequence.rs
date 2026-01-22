3888dcc8d6c57b822b38135c76c77cdc16c2e935
Sam El-Kamand <sam.elkamand@gmail.com>
2026-01-22 16:17:31 +1100

    feat: updated example to include subseq

--- examples/read_sequence.rs
+++ examples/read_sequence.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn main() -> Result<(), seqlib::errors::SeqError> {
     let seq = seqlib::sequences::DnaSeq::new("AGACT").unwrap();
     eprintln!("{} Sequence: {}", seq.alphabet(), seq);
     eprintln!("Length: {} nucleotides", seq.len());
@@ -14,4 +14,14 @@ fn main() {
 
     // Reverse Complement
     eprintln!("{}", seq.reverse_complement().describe());
+
+    // Subsample
+    let subseq = seq.subseq(0, 3).unwrap();
+
+    eprintln!("Subseq 0-3: {subseq}");
+
+    let illegal_subseq = seq.subseq(3, 0)?;
+
+    eprintln!("{illegal_subseq}");
+    Ok(())
 }
