f2b00b2fc65369522057c7dca62b346e72d8cae3
selkamand <sam.elkamand@gmail.com>
2026-09-19 13:04:46 +1000

    docs: update examples with new function names

--- examples/highlight_sequences.rs
+++ examples/highlight_sequences.rs
@@ -11,12 +11,15 @@ fn main() {
     // 0-based)
     println!("Highlight the 4th base in the sequence (with text)");
     let position = basepos!(4);
-    println!("{}", seq.format_with_highlight_pos(Some(position)));
+    println!("{}", seq.format_with_highlighted_basepos(Some(position)));
 
     // Highlight the 2nd-4th base (1-based; both-end inclusive)
-    println!("\nHighlight the interval: 2nd-4th (1-based both-end inclusive) with square brakcets");
+    println!("\nHighlight the interval: 2nd-4th (1-based both-end inclusive) with square brackets");
     let interval = BaseInterval::new(basepos!(2), basepos!(4)).unwrap();
-    println!("{}", seq.format_with_highlight_interval(Some(&interval)));
+    println!(
+        "{}",
+        seq.format_with_highlighted_base_interval(Some(&interval))
+    );
 
     println!("\nHighlight the interval: 2nd-4th (1-based both-end inclusive) using ansi colour");
     println!("{}", seq.format_with_coloured_interval(&interval));
@@ -29,7 +32,10 @@ fn main() {
         BasePos::new(100usize).unwrap(),
     )
     .unwrap();
-    println!("{}", seq.format_with_highlight_interval(Some(&interval2)));
+    println!(
+        "{}",
+        seq.format_with_highlighted_base_interval(Some(&interval2))
+    );
 
     println!("\nHighlight the 5th-100th base with ansi colours:");
     println!("{}", seq.format_with_coloured_interval(&interval2));
